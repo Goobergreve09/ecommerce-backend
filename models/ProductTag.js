1fc0b90e449ca76e14a436d36ef6d8516d12cc61
Goobergreve09 <gregory.greve@yahoo.com>
2024-01-07 18:02:24 -0500

    models completed

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product', // Name of the referenced model
+        key: 'id', // Name of the referenced key
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag', 
+        key: 'id', 
+      },
+    },
   },
   {
     sequelize,
